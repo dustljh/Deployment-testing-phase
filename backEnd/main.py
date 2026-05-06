c06f86a2ae1311e33d05925e5a0c59562ae15bc5
dustljh <ij66724@gmail.com>
2026-05-07 00:40:28 +0900

    add

--- backEnd/main.py
+++ backEnd/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://deployment-testing-phase.vercel.app"],
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
