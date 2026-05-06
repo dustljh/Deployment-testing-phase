e701975b8ea2a6fce38b010e22cf595d26f5e534
dustljh <ij66724@gmail.com>
2026-05-07 01:40:49 +0900

    a

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,16 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import List
-from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "https://deployment-testing-phase.vercel.app",
-        "http://localhost:5173"
-    ],
+    allow_origins=["*"], # 테스트를 위해 일시적으로 모든 주소 허용
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -22,11 +19,15 @@ class User(BaseModel):
 
 db = []
 
+@app.get("/")
+def read_root():
+    return {"message": "Server is running!"}
+
 @app.post("/users")
 def create_user(user: User):
     db.append(user)
-    return {"message": f"{user.name} 등록 성공!", "data": user}
+    return {"message": "Success", "data": user}
 
-@app.get("/users", response_model=List[User])
+@app.get("/users")
 def get_users():
     return db
